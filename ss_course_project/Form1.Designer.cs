5429722ac088fa18eca4f72e3beafcf8b9c80e77
Sergey Kostyuk <contact@s-kostyuk.me>
2017-06-02 19:15:36 +0300

    Dummy card interface

---
+++ ss_course_project/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace ss_course_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelCards = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panelCards.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelCards
+            // 
+            this.panelCards.Controls.Add(this.panel1);
+            this.panelCards.Controls.Add(this.panel2);
+            this.panelCards.Controls.Add(this.panel3);
+            this.panelCards.Controls.Add(this.panel4);
+            this.panelCards.Controls.Add(this.panel5);
+            this.panelCards.Controls.Add(this.panel6);
+            this.panelCards.Controls.Add(this.panel7);
+            this.panelCards.Controls.Add(this.panel8);
+            this.panelCards.Location = new System.Drawing.Point(0, 0);
+            this.panelCards.Name = "panelCards";
+            this.panelCards.Size = new System.Drawing.Size(566, 261);
+            this.panelCards.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(150, 86);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Window;
+            this.panel2.Location = new System.Drawing.Point(159, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(150, 86);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.Window;
+            this.panel3.Location = new System.Drawing.Point(315, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(150, 86);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.Window;
+            this.panel4.Location = new System.Drawing.Point(3, 95);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(150, 86);
+            this.panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.SystemColors.Window;
+            this.panel5.Location = new System.Drawing.Point(159, 95);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(150, 86);
+            this.panel5.TabIndex = 4;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.SystemColors.Window;
+            this.panel6.Location = new System.Drawing.Point(315, 95);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(150, 86);
+            this.panel6.TabIndex = 5;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.SystemColors.Window;
+            this.panel7.Location = new System.Drawing.Point(3, 187);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(150, 86);
+            this.panel7.TabIndex = 6;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.SystemColors.Window;
+            this.panel8.Location = new System.Drawing.Point(159, 187);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(150, 86);
+            this.panel8.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 261);
+            this.Controls.Add(this.panelCards);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panelCards.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel panelCards;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+    }
+}
+
