a53a620b096c50d3776e76ca6fc8cabafc0d37e4
Sergey Kostyuk <contact@s-kostyuk.me>
2017-06-05 20:41:32 +0300

    Simple MQTT subscriber

--- ss_course_project.test_app/Program.cs
+++ ss_course_project.test_app/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace ss_course_project.test_app
 {
@@ -10,6 +12,22 @@ namespace ss_course_project.test_app
     {
         static void Main(string[] args)
         {
+            MqttClient client = new MqttClient("....");
+            client.Connect("Sensor-Board");
+
+            client.Subscribe(new string[]{ "/sensors/TEMP1"}, new byte[]{ MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+
+            client.MqttMsgPublishReceived += Client_MqttMsgPublishReceived;
+        }
+
+        private static void Client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            Console.WriteLine(string.Format("\nNew message received\n{0}", DateTime.Now));
+            Console.WriteLine(
+                string.Format("topic: {0}\nmessage: {1}",
+                    e.Topic, System.Text.Encoding.UTF8.GetString(e.Message)
+                )
+                );
         }
     }
 }
