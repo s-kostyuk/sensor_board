3779202e6da65cbd9b9335e675bf053ce729a45c
Sergey Kostyuk <contact@s-kostyuk.me>
2017-06-05 14:11:47 +0300

    Dynamic card addition

--- ss_course_project/Form1.Designer.cs
+++ ss_course_project/Form1.Designer.cs
@@ -29,102 +29,35 @@
         private void InitializeComponent()
         {
             this.panelCards = new System.Windows.Forms.FlowLayoutPanel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.panel6 = new System.Windows.Forms.Panel();
-            this.panel7 = new System.Windows.Forms.Panel();
-            this.panel8 = new System.Windows.Forms.Panel();
+            this.panelAddCard = new System.Windows.Forms.Panel();
             this.panelCards.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelCards
             // 
-            this.panelCards.Controls.Add(this.panel1);
-            this.panelCards.Controls.Add(this.panel2);
-            this.panelCards.Controls.Add(this.panel3);
-            this.panelCards.Controls.Add(this.panel4);
-            this.panelCards.Controls.Add(this.panel5);
-            this.panelCards.Controls.Add(this.panel6);
-            this.panelCards.Controls.Add(this.panel7);
-            this.panelCards.Controls.Add(this.panel8);
+            this.panelCards.Controls.Add(this.panelAddCard);
+            this.panelCards.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelCards.Location = new System.Drawing.Point(0, 0);
             this.panelCards.Name = "panelCards";
-            this.panelCards.Size = new System.Drawing.Size(566, 261);
+            this.panelCards.Size = new System.Drawing.Size(501, 312);
             this.panelCards.TabIndex = 0;
             // 
-            // panel1
+            // panelAddCard
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.Window;
-            this.panel1.Location = new System.Drawing.Point(3, 3);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(150, 86);
-            this.panel1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.SystemColors.Window;
-            this.panel2.Location = new System.Drawing.Point(159, 3);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(150, 86);
-            this.panel2.TabIndex = 1;
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.SystemColors.Window;
-            this.panel3.Location = new System.Drawing.Point(315, 3);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(150, 86);
-            this.panel3.TabIndex = 2;
-            // 
-            // panel4
-            // 
-            this.panel4.BackColor = System.Drawing.SystemColors.Window;
-            this.panel4.Location = new System.Drawing.Point(3, 95);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(150, 86);
-            this.panel4.TabIndex = 3;
-            // 
-            // panel5
-            // 
-            this.panel5.BackColor = System.Drawing.SystemColors.Window;
-            this.panel5.Location = new System.Drawing.Point(159, 95);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(150, 86);
-            this.panel5.TabIndex = 4;
-            // 
-            // panel6
-            // 
-            this.panel6.BackColor = System.Drawing.SystemColors.Window;
-            this.panel6.Location = new System.Drawing.Point(315, 95);
-            this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(150, 86);
-            this.panel6.TabIndex = 5;
-            // 
-            // panel7
-            // 
-            this.panel7.BackColor = System.Drawing.SystemColors.Window;
-            this.panel7.Location = new System.Drawing.Point(3, 187);
-            this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(150, 86);
-            this.panel7.TabIndex = 6;
-            // 
-            // panel8
-            // 
-            this.panel8.BackColor = System.Drawing.SystemColors.Window;
-            this.panel8.Location = new System.Drawing.Point(159, 187);
-            this.panel8.Name = "panel8";
-            this.panel8.Size = new System.Drawing.Size(150, 86);
-            this.panel8.TabIndex = 7;
-            this.panel8.DoubleClick += new System.EventHandler(this.panel8_DoubleClick);
+            this.panelAddCard.BackColor = System.Drawing.SystemColors.Window;
+            this.panelAddCard.BackgroundImage = global::ss_course_project.Properties.Resources.course_plus_sign;
+            this.panelAddCard.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panelAddCard.Location = new System.Drawing.Point(3, 3);
+            this.panelAddCard.Name = "panelAddCard";
+            this.panelAddCard.Size = new System.Drawing.Size(144, 86);
+            this.panelAddCard.TabIndex = 0;
+            this.panelAddCard.Click += new System.EventHandler(this.panelAddCard_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(566, 261);
+            this.ClientSize = new System.Drawing.Size(501, 312);
             this.Controls.Add(this.panelCards);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -136,14 +69,7 @@
         #endregion
 
         private System.Windows.Forms.FlowLayoutPanel panelCards;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Panel panel5;
-        private System.Windows.Forms.Panel panel6;
-        private System.Windows.Forms.Panel panel7;
-        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panelAddCard;
     }
 }
 
--- ss_course_project/Form1.cs
+++ ss_course_project/Form1.cs
@@ -8,23 +8,53 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*****************************************************************************/
+
 namespace ss_course_project
 {
     public partial class Form1 : Form
     {
+        /*-------------------------------------------------------------------*/
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void panel8_DoubleClick(object sender, EventArgs e)
+        /*-------------------------------------------------------------------*/
+
+        private void set_standart_panel_props(Panel panel, int index = 0)
         {
-            var new_size = this.panel1.Size;
-
-            new_size.Width = new_size.Width * 3;
-            new_size.Height = new_size.Height * 2;
-
-            this.panel1.Size = new_size;
+            panel.BackColor = System.Drawing.SystemColors.Window;
+            panel.Name = string.Format("panel{0}", index);
+            panel.Size = new System.Drawing.Size(144, 86);
+            panel.TabIndex = index;
         }
+
+        /*-------------------------------------------------------------------*/
+
+        private void movePanelAddCard(int new_index)
+        {
+            this.panelAddCard.TabIndex = new_index;
+            this.panelCards.Controls.SetChildIndex(panelAddCard, new_index);
+        }
+
+        /*-------------------------------------------------------------------*/
+
+        private void panelAddCard_Click(object sender, EventArgs e)
+        {
+            int new_index = this.panelCards.Controls.Count - 1;
+
+            Panel new_panel = new Panel();
+            set_standart_panel_props(new_panel, new_index);
+
+            this.panelCards.Controls.Add(new_panel);
+
+            movePanelAddCard(new_index + 1);
+        }
+
+        /*-------------------------------------------------------------------*/
     }
 }
+
+/*****************************************************************************/
