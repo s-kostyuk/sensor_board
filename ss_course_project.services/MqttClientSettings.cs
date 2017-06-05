664720e4c010452dc209f08e994c1c9fa5fca217
Sergey Kostyuk <contact@s-kostyuk.me>
2017-06-05 22:58:24 +0300

    Settings

---
+++ ss_course_project.services/MqttClientSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ss_course_project.services
+{
+    /// <summary>
+    /// ОЧЕНЬ базовые настройки MQTT-клиента
+    /// </summary>
+    public struct MqttClientSettings
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string Id { get; set; }
+        public string User { get; set; }
+        public string Password { get; set; }
+    }
+}
