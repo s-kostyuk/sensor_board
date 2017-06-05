e8cd08f3c656a0d819d3b110bfa88ca5d2af62c3
Sergey Kostyuk <contact@s-kostyuk.me>
2017-06-05 14:48:26 +0300

    Filled cards with temp and name, implemented updating

--- ss_course_project/Form1.Designer.cs
+++ ss_course_project/Form1.Designer.cs
@@ -30,16 +30,19 @@
         {
             this.panelCards = new System.Windows.Forms.FlowLayoutPanel();
             this.panelAddCard = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.updateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.panelCards.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelCards
             // 
             this.panelCards.Controls.Add(this.panelAddCard);
             this.panelCards.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelCards.Location = new System.Drawing.Point(0, 0);
+            this.panelCards.Location = new System.Drawing.Point(0, 24);
             this.panelCards.Name = "panelCards";
-            this.panelCards.Size = new System.Drawing.Size(501, 312);
+            this.panelCards.Size = new System.Drawing.Size(501, 288);
             this.panelCards.TabIndex = 0;
             // 
             // panelAddCard
@@ -53,16 +56,38 @@
             this.panelAddCard.TabIndex = 0;
             this.panelAddCard.Click += new System.EventHandler(this.panelAddCard_Click);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.updateToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(501, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // updateToolStripMenuItem
+            // 
+            this.updateToolStripMenuItem.Name = "updateToolStripMenuItem";
+            this.updateToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.updateToolStripMenuItem.Text = "Update";
+            this.updateToolStripMenuItem.Click += new System.EventHandler(this.updateToolStripMenuItem_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(501, 312);
             this.Controls.Add(this.panelCards);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.panelCards.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +95,8 @@
 
         private System.Windows.Forms.FlowLayoutPanel panelCards;
         private System.Windows.Forms.Panel panelAddCard;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem updateToolStripMenuItem;
     }
 }
 
--- ss_course_project/Form1.cs
+++ ss_course_project/Form1.cs
@@ -23,12 +23,57 @@ namespace ss_course_project
 
         /*-------------------------------------------------------------------*/
 
+        private void fill_standart_panel(Panel panel, string cardName = "Card Name")
+        {
+            Label labelValue = new System.Windows.Forms.Label();
+            Label labelCardName = new System.Windows.Forms.Label();
+
+            // 
+            // labelCardName
+            // 
+            labelCardName.Anchor = ((AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            labelCardName.Location = new System.Drawing.Point(53, 73);
+            labelCardName.Name = "labelCardName";
+            labelCardName.Size = new System.Drawing.Size(88, 13);
+            labelCardName.TabIndex = 1;
+            labelCardName.Text = "Card Name";
+            labelCardName.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // labelValue
+            // 
+            labelValue.BackColor = System.Drawing.Color.Transparent;
+            labelValue.Dock = System.Windows.Forms.DockStyle.Fill;
+            labelValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            labelValue.Location = new System.Drawing.Point(0, 0);
+            labelValue.Name = "labelValue";
+            labelValue.Size = new System.Drawing.Size(144, 86);
+            labelValue.TabIndex = 0;
+            labelValue.Text = "Null";
+            labelValue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+            panel.Controls.Add(labelCardName);
+            panel.Controls.Add(labelValue);
+        }
+
+        /*-------------------------------------------------------------------*/
+
         private void set_standart_panel_props(Panel panel, int index = 0)
         {
             panel.BackColor = System.Drawing.SystemColors.Window;
             panel.Name = string.Format("panel{0}", index);
             panel.Size = new System.Drawing.Size(144, 86);
             panel.TabIndex = index;
+        }
+
+        /*-------------------------------------------------------------------*/
+
+        private Panel get_standart_panel(int index = 0)
+        {
+            Panel new_panel = new Panel();
+            set_standart_panel_props(new_panel, index);
+            fill_standart_panel(new_panel);
+
+            return new_panel;
         }
 
         /*-------------------------------------------------------------------*/
@@ -45,13 +90,31 @@ namespace ss_course_project
         {
             int new_index = this.panelCards.Controls.Count - 1;
 
-            Panel new_panel = new Panel();
-            set_standart_panel_props(new_panel, new_index);
+            Panel new_panel = get_standart_panel(new_index);
 
             this.panelCards.Controls.Add(new_panel);
 
             movePanelAddCard(new_index + 1);
         }
+
+        /*-------------------------------------------------------------------*/
+
+        private void updateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (Panel p in this.panelCards.Controls)
+            {
+                if (p == this.panelAddCard)
+                {
+                    break;
+                }
+
+                p.Controls["labelValue"].Text = string.Format("{0} °C", rand.Next(20, 31));
+            } 
+        }
+
+        /*-------------------------------------------------------------------*/
+
+        Random rand = new Random();
 
         /*-------------------------------------------------------------------*/
     }
