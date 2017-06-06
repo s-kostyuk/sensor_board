7d983bf74dd73a2be8840e5108c5c7ec88a8484f
Sergey Kostyuk <contact@s-kostyuk.me>
2017-06-07 02:19:10 +0300

    A DRAFT of the SensorsRepository

---
+++ ss_course_project.services/Repositories/SensorsRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ss_course_project.services.Model;
+
+/*****************************************************************************/
+
+namespace ss_course_project.services.Repositories
+{
+    public class SensorsRepository
+    {
+        /*-------------------------------------------------------------------*/
+
+        public HashSet<MqttTempSensor> Sensors { get; private set; }
+
+        /*-------------------------------------------------------------------*/
+
+        public SensorsRepository()
+        {
+            Sensors = new HashSet<MqttTempSensor>();
+        }
+
+        /*-------------------------------------------------------------------*/
+
+
+
+        /*-------------------------------------------------------------------*/
+    }
+}
+
+/*****************************************************************************/
