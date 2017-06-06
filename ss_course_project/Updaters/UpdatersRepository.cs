1b92609aace89339e4b8462fe20dfd80c62a505f
Sergey Kostyuk <contact@s-kostyuk.me>
2017-06-07 02:11:26 +0300

    A DRAFT of the UpdatersRepository

---
+++ ss_course_project/Updaters/UpdatersRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*****************************************************************************/
+
+namespace ss_course_project.gui.Updaters
+{
+    public class UpdatersRepository
+    {
+        /*-------------------------------------------------------------------*/
+
+        public HashSet<PanelSensorUpdater> PanelSensorUpdaters { get; private set; }
+
+        /*-------------------------------------------------------------------*/
+
+        public UpdatersRepository()
+        {
+            PanelSensorUpdaters = new HashSet<PanelSensorUpdater>();
+        }
+
+        /*-------------------------------------------------------------------*/
+
+
+
+        /*-------------------------------------------------------------------*/
+    }
+}
+
+/*****************************************************************************/
