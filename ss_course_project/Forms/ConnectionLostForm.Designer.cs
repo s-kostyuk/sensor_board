dd5937f157877bc234fb07fe5a8646b97a2bf471
Sergey Kostyuk <contact@s-kostyuk.me>
2017-06-09 11:43:26 +0300

    Added lost form. Decreased memory usage. Yes, in a hack

---
+++ ss_course_project/Forms/ConnectionLostForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ss_course_project.gui.Forms
+{
+    partial class ConnectionLostForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.buttonTryAgain = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.Location = new System.Drawing.Point(34, 59);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(136, 13);
+            this.labelMessage.TabIndex = 0;
+            this.labelMessage.Text = "Connection lost. Try again?";
+            // 
+            // buttonTryAgain
+            // 
+            this.buttonTryAgain.Location = new System.Drawing.Point(12, 124);
+            this.buttonTryAgain.Name = "buttonTryAgain";
+            this.buttonTryAgain.Size = new System.Drawing.Size(75, 23);
+            this.buttonTryAgain.TabIndex = 2;
+            this.buttonTryAgain.Text = "Try Again";
+            this.buttonTryAgain.UseVisualStyleBackColor = true;
+            this.buttonTryAgain.Click += new System.EventHandler(this.buttonTryAgain_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(119, 124);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 3;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // ConnectionLostForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(206, 159);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonTryAgain);
+            this.Controls.Add(this.labelMessage);
+            this.Name = "ConnectionLostForm";
+            this.Text = "ConnectionLostForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelMessage;
+        private System.Windows.Forms.Button buttonTryAgain;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
---
+++ ss_course_project/Forms/ConnectionLostForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ss_course_project.gui.Forms
+{
+    public partial class ConnectionLostForm : Form
+    {
+        public ConnectionLostForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonTryAgain_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Retry;
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Abort;
+        }
+    }
+}
